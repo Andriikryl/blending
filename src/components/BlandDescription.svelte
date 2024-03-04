9d2f4c729003f3d0e3d4cd9ce00d112e91cfcf1a
Andriikryl <andreyk557@gmail.com>
2024-03-04 11:36:59 +0200

    add BlandDescription component

---
+++ src/components/BlandDescription.svelte
@@ -0,0 +1,18 @@
+<script lang="ts"></script>
+
+<p class="description">
+  <slot />
+</p>
+
+<style>
+  .description {
+    color: var(--black, #1b1b1b);
+    font-family: Patrick Hand;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 28px;
+    border-left: 3px solid #ffc300;
+    padding-inline-start: 10px;
+  }
+</style>
