4bdaf5033a5a6e5d24da4f6f806d1c9ab7e6841d
Andriikryl <andreyk557@gmail.com>
2024-03-04 13:38:38 +0200

    add FinalBackdrop component

---
+++ src/components/FinalBackdrop.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import BlandDescription from "./BlandDescription.svelte";
+  export let bkR: number;
+  export let bkG: number;
+  export let bkB: number;
+</script>
+
+<div class="backdrop__box">
+  <BlandDescription>Backdrop final color</BlandDescription>
+  <div class="box" style="background-color: rgb({bkR}, {bkG}, {bkB});"></div>
+</div>
+
+<style>
+  .backdrop__box {
+    margin-block-start: 40px;
+  }
+  .box {
+    width: 100px;
+    height: 100px;
+    margin-block-start: 10px;
+  }
+</style>
