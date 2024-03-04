b7ad0ea0b87902b0b25969e89b999deb55b83028
Andriikryl <andreyk557@gmail.com>
2024-03-04 11:37:45 +0200

    add BlandExemple component

---
+++ src/components/BlandExemple.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  export let guiBg;
+  export let gui;
+</script>
+
+<div class="wrapper">
+  <div
+    class="box"
+    style="background-color: rgb({$guiBg.R}, {$guiBg.G}, {$guiBg.B});"
+  ></div>
+  <div
+    class="boxBg"
+    style="background-color: rgb({$gui.R}, {$gui.G}, {$gui.B});"
+  ></div>
+</div>
+
+<style>
+  .wrapper {
+    display: flex;
+    justify-content: center;
+  }
+  .box {
+    width: 100px;
+    height: 100px;
+  }
+  .boxBg {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    position: relative;
+    left: -54px;
+    bottom: -40px;
+    z-index: 10;
+    mix-blend-mode: darken;
+  }
+</style>
