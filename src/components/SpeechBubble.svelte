0a5efc81f3f46f1f180eaa4fd1b56a77c9ad1d4e
Andriikryl <andreyk557@gmail.com>
2024-03-05 18:31:00 +0200

    add SpeechBuble component

---
+++ src/components/SpeechBubble.svelte
@@ -0,0 +1,22 @@
+<script lang="ts"></script>
+
+<div class="speech-bubble">
+  <slot />
+</div>
+
+<style>
+  .speech-bubble {
+    position: relative;
+  }
+  .speech-bubble::before {
+    content: "";
+
+    position: absolute;
+    bottom: 0;
+    left: -1rem;
+
+    border-width: 1rem;
+    border-style: solid;
+    border-color: transparent transparent rgb(226 232 240) transparent;
+  }
+</style>
