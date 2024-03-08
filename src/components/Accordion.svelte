8b3330e22bc916ef9650ff2db8e1c5c60537caaa
Andriikryl <andreyk557@gmail.com>
2024-03-08 15:58:22 +0200

    add Accordion component

---
+++ src/components/Accordion.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { setAccordionOptions } from "./store";
+  export let collapse = false;
+  setAccordionOptions({ collapse });
+</script>
+
+<div class="accordion">
+  <slot />
+</div>
+
+<style>
+  .accordion {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: var(--accordion-width, 100%);
+    padding: var(--accordion-padding, 1rem);
+    color: var(--accordion-color, hsl(220 10% 98%));
+    background-color: transparent;
+  }
+</style>
--- src/components/store.ts
+++ src/components/store.ts
@@ -1,5 +1,24 @@
 import { writable } from 'svelte/store'
+import { setContext, getContext } from 'svelte'
+import type {
+	AccordionOptions,
+	ActiveId,
+	ActiveIdContext,
+	CollapseContext
+} from './storeTypes'
 
 export function guiControls(values: any) {
 	return writable(values)
+}
+
+export function setAccordionOptions({ collapse }: AccordionOptions) {
+	const activeComponentId = writable<ActiveId>(null)
+	setContext<CollapseContext>('collapse', collapse)
+	setContext<ActiveIdContext>('active', activeComponentId)
+}
+
+export function getAccordionOptions() {
+	const collapse = getContext<CollapseContext>('collapse')
+	const activeComponentId = getContext<ActiveIdContext>('active')
+	return { collapse, activeComponentId }
 }
