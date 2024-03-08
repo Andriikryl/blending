646b3fa44e3d3a135f59738401180fcd6d4100f3
Andriikryl <andreyk557@gmail.com>
2024-03-09 01:11:44 +0200

    fix text bug

--- src/components/Screen.svelte
+++ src/components/Screen.svelte
@@ -63,14 +63,7 @@
       </div>
     </svelte:fragment>
     <svelte:fragment slot="item-end">
-      <BlandDescription
-        >Multiplies the complements of the backdrop and source color values,
-        then complements the result. The result color is always at least as
-        light as either of the two constituent colors. Screening any color with
-        white produces white; screening with black leaves the original color
-        unchanged. The effect is similar to projecting multiple photographic
-        slides simultaneously onto a single screen.</BlandDescription
-      >
+      <BlandDescription>Algorithm</BlandDescription>
       <div class="flex__group">
         <div
           class="iiner__box"
